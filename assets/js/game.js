ade6d891643bef189f48dc434a7aaf1905e91d74
Izzy <isaiah.ozuna@yahoo.com>
2022-04-11 15:46:50 -0500

    Added some JS to the page about console log and var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?");
+// Note the lack of question marks around playerName
+//window.alert(playerName);
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+//this will do math and log 20
+console.log(10 + 10);
+
+//what is this?
+console.log("Our robot's name is" + playerName);
+
+
+
+// This creates a function called 'fight'
+function fight() {
+    window.alert("The fight had begun!");
+}
+//fight();
