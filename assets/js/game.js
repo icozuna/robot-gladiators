2a7a216f2b0e1fab52fc95d23a101ac7a6f0c480
Izzy <isaiah.ozuna@yahoo.com>
2022-04-12 16:03:11 -0500

    added enemy combatants and added for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,18 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-//Tou can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trouble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//Game States
+//"WIN" - Player robot has defeated all enemy-robots
+//  * Fight all enemy robots
+//  *Defeat each enemy-robot
+//"LOSE" - Player robot's health is zero or less
+
 // fight function
-var fight = function () {
+var fight = function (enemyName) {
   //Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -81,4 +84,6 @@ var fight = function () {
   }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
